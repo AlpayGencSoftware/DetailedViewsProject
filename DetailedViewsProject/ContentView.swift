3beebf645813b709f03881557bd7829afd1f3374
AlpayGencSoftware <gencoalpay@gmail.com>
2023-09-05 15:08:48 +0300

    Family photos added

--- DetailedViewsProject/ContentView.swift
+++ DetailedViewsProject/ContentView.swift
@@ -9,13 +9,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        VStack(alignment: .center){
+            VStack{
+                Image("family").resizable().aspectRatio(contentMode:.fit)
+                    .frame(
+                        width:UIScreen.main.bounds.width * 0.9,
+                        height:UIScreen.main.bounds.height * 0.38).clipShape(Rectangle()).overlay(Rectangle().stroke(Color.white, lineWidth: 6))
+                    .shadow(radius: 10)
+            }
+            HStack(alignment:.center){
+                Text("Hello Swift")
+                    .foregroundColor(Color.orange)
+                    .background(Color.black)
+                    .font(.largeTitle)
+                    .fontWeight(.light)
+               .frame(width: 100, height: 100)
+                Text("Can and Umay")
+                    .foregroundColor(Color.red)
+                    .background(Color.orange)
+                    .font(.largeTitle)
+                .frame(width: 200, height: 100)
+            }
         }
-        .padding()
+        
     }
 }
 
