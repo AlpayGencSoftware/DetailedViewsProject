03c11b7d7b0b12549ef802ea742d964df1a9780f
AlpayGencSoftware <gencoalpay@gmail.com>
2023-09-05 13:15:18 +0300

    Initial Commit

---
+++ DetailedViewsProject/DetailedViewsProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  DetailedViewsProjectApp.swift
+//  DetailedViewsProject
+//
+//  Created by AlpayGenc on 5.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct DetailedViewsProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
